0948850aa70ab05d5b11e60ddd73fec02f6cca5a
Zhovtaniuk Maxym <zhovtyjshady@gmail.com>
2023-02-16 15:13:42 +0200

    readme refactored and form validation added

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -6,13 +6,30 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent implements OnInit{
+export class FormComponent implements OnInit {
   reactiveForm: FormGroup;
 
   ngOnInit() {
     this.reactiveForm = new FormGroup<any>({
-      password: new FormControl(null, [Validators.required, Validators.minLength(8)])
+      password: new FormControl(null, [Validators.required])
     })
   }
 
+  getColor(): [object, object, object] {
+    const passwordValue = this.reactiveForm.controls["password"].value
+    const regexEasyPassword = /^[a-zA-Z]+$|^[\d]+$|^[\W_]+$/i
+    const regexStrongPassword = /(?=.*[a-zA-Z])(?=.*[\W._])(?=.*[\d])/i
+
+    if (!passwordValue) {
+      return [{"background-color": "grey"}, {"background-color": "grey"}, {"background-color": "grey"}]
+    } else if (passwordValue.length < 8) {
+      return [{"background-color": "red"}, {"background-color": "red"}, {"background-color": "red"}]
+    } else if (regexEasyPassword.test(passwordValue)) {
+      return [{"background-color": "red"}, {"background-color": "grey"}, {"background-color": "grey"}]
+    } else if (regexStrongPassword.test(passwordValue)) {
+      return [{"background-color": "green"}, {"background-color": "green"}, {"background-color": "green"}]
+    }
+
+    return [{"background-color": "yellow"}, {"background-color": "yellow"}, {"background-color": "grey"}]
+  }
 }
